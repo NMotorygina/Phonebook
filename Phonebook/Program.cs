e7f67732f2c2ffa47d7ed12083545f9e1ee85c30
Nataliya <nmotorygina@yandex.ru>
2021-07-18 00:44:44 +0300

    Initial commit

---
+++ Phonebook/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace Phonebook
+{
+    class Program
+    {
+        static void Main()
+        {
+            var connection = "Data Source = Phonebook.sqlite; Mode = ReadWrite;";
+            var db = new SqliteConnection(connection);
+            db.Open();
+            
+            string sql, first_name, last_name;
+            int phone_number;
+            
+            Console.Write("Введите имя - ");
+            first_name = Console.ReadLine();
+            Console.Write("Введите фамилию - ");
+            last_name = Console.ReadLine();
+            Console.Write("Введите телефон - ");
+            phone_number = Convert.ToInt32(Console.ReadLine());
+            
+            sql = $"insert into table_phonebook (first_name, last_name, phone_number) values ('{first_name}', '{last_name}', {phone_number})";
+            var command = new SqliteCommand {Connection = db, CommandText = sql};
+            command.ExecuteNonQuery();
+            
+            db.Close();
+        }
+    }
+}
